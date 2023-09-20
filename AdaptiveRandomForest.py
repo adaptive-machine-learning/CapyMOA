14c0fe3024d0a86fc704ed0ee051a8f096ce51a8
Heitor <heitor_murilo_gomes@yahoo.com.br>
2023-09-20 18:59:01 +1200

    Adding evaluation wrappers and an experimental version of an ARF wrapper

---
+++ AdaptiveRandomForest.py
@@ -0,0 +1,44 @@
+from prepare_jpype import start_jpype
+import pandas as pd
+start_jpype()
+
+# Prepare examples and a __main__
+# Should add Wrapper at the end or find a way to use the same name as in MOA? 
+
+from moa.classifiers.meta import AdaptiveRandomForest
+
+class AdaptiveRandomForestWrapper:
+    def __init__(self, stream_context, tree_learner=None, ensemble_size=100, 
+                 m_features_mode=None, m_features_per_tree_size=60, lambda_param=6.0,
+                number_of_jobs=1, drift_detection_method=None, warning_detection_method=None,
+                disable_weighted_vote=False, disable_drift_detection=False, disable_background_learner=False):
+        # Initialize instance attributes with default values        
+        self.tree_learner = "(ARFHoeffdingTree -e 2000000 -g 50 -c 0.01)" if tree_learner is None else tree_learner
+        self.ensemble_size = ensemble_size
+        self.m_features_mode = "(Percentage (M * (m / 100)))" if m_features_mode is None else m_features_mode
+        self.m_features_per_tree_size = m_features_per_tree_size
+        self.lambda_param = lambda_param
+        self.number_of_jobs = number_of_jobs
+        self.drift_detection_method = "(ADWINChangeDetector -a 1.0E-3)" if drift_detection_method is None else drift_detection_method
+        self.warning_detection_method = "(ADWINChangeDetector -a 1.0E-2)" if warning_detection_method is None else warning_detection_method
+        self.disable_weighted_vote = disable_weighted_vote
+        self.disable_drift_detection = disable_drift_detection
+        self.disable_background_learner = disable_background_learner
+
+        self.moa_learner = AdaptiveRandomForest()
+        # self.moa_learner.getOptions().setViaCLIString(f"-l {self.tree_learner} -s {self.ensemble_size}" -o {self.m_features_mode} -m {self.m_features_per_tree_size} -a {self.lambda} -j {self.number_of_jobs} -x {self.drift_detection_method} -p {self.warning_detection_method} {'-w' if self.disable_weighted_vote else ''} {'-u' if self.disable_drift_detection else ''}  {'-q' if self.disable_background_learner else ''}")
+        self.moa_learner.getOptions().setViaCLIString(f"-l {self.tree_learner} -s {self.ensemble_size} -o {self.m_features_mode} -m {self.m_features_per_tree_size} -a {self.lambda_param} -j {self.number_of_jobs} -x {self.drift_detection_method} -p {self.warning_detection_method} {'-w' if self.disable_weighted_vote else ''} {'-u' if self.disable_drift_detection else ''}  {'-q' if self.disable_background_learner else ''}")
+        # self.moa_learner.setRandomSeed(1)
+        self.moa_learner.prepareForUse()
+        self.moa_learner.resetLearningImpl()
+
+        self.moa_learner.setModelContext(stream_context)
+
+    def train(self, instance):
+        self.moa_learner.trainOnInstance(instance)
+
+    def predict(self, instance):
+        return Utils.maxIndex(self.moa_learner.getVotesForInstance(instance))
+
+    def predict_proba(self, instance):
+        return self.moa_learner.getVotesForInstance(instance)
--- evaluation.py
+++ evaluation.py
@@ -3,26 +3,30 @@ import time
 import datetime as dt
 import resource
 import os
+import random
 
 # Create the JVM and add the MOA jar to the classpath
 import prepare_jpype
 
-# MOA imports
-from moa.classifiers.meta import AdaptiveRandomForest
-from moa.core import Example
+# MOA/Java imports
+from com.yahoo.labs.samoa.instances import Instances, Instance, Attribute, DenseInstance
+from moa.core import Example, InstanceExample
 from moa.evaluation import BasicClassificationPerformanceEvaluator, WindowClassificationPerformanceEvaluator
 from moa.streams import ArffFileStream
+from java.util import ArrayList
 
 ## River imports
 from river import stream
 from river import metrics
 
-## Functions fo measuring runtime
+
+## Functions to measure runtime
 def start_time_measuring(): 
     start_wallclock_time = time.time()
     start_cpu_time = resource.getrusage(resource.RUSAGE_SELF).ru_utime + resource.getrusage(resource.RUSAGE_SELF).ru_stime
 
     return start_wallclock_time, start_cpu_time
+
 
 def stop_time_measuring(start_wallclock_time, start_cpu_time):
     # Stop measuring time
@@ -35,8 +39,168 @@ def stop_time_measuring(start_wallclock_time, start_cpu_time):
 
     return elapsed_wallclock_time, elapsed_cpu_time
 
+
+class Schema:
+    """
+    This class is a wrapper for the MOA header, but it can be set up in Python directly by specifying the labels attribute.
+    If moa_header is specified, then it overrides everything else. 
+    In the future, we might want to have a way to specify the values for nominal attributes as well, so far just the labels. 
+    """
+    def __init__(self, moa_header=None, labels=None):   
+        self.moa_header = moa_header
+        self.label_values = labels
+        
+        if self.moa_header is not None:
+            self.label_values = [g for g in self.moa_header.outputAttribute(1).getAttributeValues()]
+            
+    def getLabelValues(self):
+        if self.label_values is None:
+            return []
+        else:
+            return self.label_values
+
+    def getLabelIndexes(self):
+        if self.label_values is None:
+            return []
+        else:
+            return list(range(len(self.label_values)))
+
+    def getMoaHeader(self):
+        return self.moa_header
+
+
+class ClassificationEvaluator: 
+    """
+    Wrapper for the Classification Performance Evaluator from MOA. By default uses the BasicClassificationPerformanceEvaluator
+    """
+    def __init__(self, schema=None, window_size=None, recall_per_class=False, precision_per_class=False, 
+                 f1_precision_recall=False, f1_per_class=False, moa_evaluator=None):
+        self.instances_seen = 0
+        self.result_windows = []
+        self.window_size = window_size
+        
+        self.moa_basic_evaluator = moa_evaluator
+        if self.moa_basic_evaluator is None:
+            self.moa_basic_evaluator = BasicClassificationPerformanceEvaluator()
+        
+        if recall_per_class: 
+            self.moa_basic_evaluator.recallPerClassOption.set()
+        if precision_per_class: 
+            self.moa_basic_evaluator.precisionPerClassOption.set()
+        if f1_precision_recall: 
+            self.moa_basic_evaluator.precisionRecallOutputOption.set()
+        if f1_per_class: 
+            self.moa_basic_evaluator.f1PerClassOption.set()
+        self.moa_basic_evaluator.prepareForUse()
+
+        _attributeValues = ArrayList()
+        self.pred_template = [0, 0]
+
+        self.schema = schema
+        if self.schema is not None:
+            self.pred_template = [0] * len(self.schema.getLabelIndexes())
+            for value in self.schema.getLabelIndexes():
+                _attributeValues.append(value)
+        else:
+            raise ValueError("Schema cannot be None, please define a schema.")
+        
+        _classAttribute = Attribute("Class", _attributeValues)
+        
+        attSub = ArrayList()
+        attSub.append(_classAttribute)
+        # If there is no moa_header specified, then it will create one
+        self._header = None
+        if self.schema is not None and self.schema.getMoaHeader() is not None:
+            self._header = self.schema.getMoaHeader()
+        else:
+            self._header = Instances("", attSub, 1)
+            self._header.setClassIndex(0);
+
+        # Create the denseInstance just once and keep reusing it by changing the classValue (more efficient). 
+        self._instance = DenseInstance(1.0, [0])
+        self._instance.setDataset(self._header)
+    
+    def update(self, y, y_pred):
+        if self.schema.getLabelIndexes() is not None:
+            # Check if y is a value present in the labels array, if yes, then convert it to a index on that list. 
+            if y in self.schema.getLabelValues():
+                y = self.schema.getLabelValues().index(y)
+            
+        # Notice, in MOA the class value is a index, not the actual value (e.g. not "one" but 0 assuming labels=["one", "two"])
+        self._instance.setClassValue(y)
+        example = InstanceExample(self._instance)
+        
+        # Shallow copy of the pred_template
+        # MOA evaluator accepts the result of getVotesForInstance which is similar to a predict_proba 
+        #    (may or may not be normalised, but for our purposes it doesn't matter)
+        prediction_array = self.pred_template[:]
+        
+        # if y_pred is None, it indicates the learner absented from predicting this instace, count as an error
+        if y_pred is None:
+            # Set y_pred to any valid prediction that is not y (force an incorrect prediction)
+            # This does not affect recall or any other metrics, because the selected value is always incorrect. 
+
+            # Create an intermediary array with indices excluding the y
+            indexesWithoutY = [i for i in range(len(self.schema.getLabelIndexes())) if i != y]
+            random_y_pred = random.choice(indexesWithoutY)
+            y_pred = self.schema.getLabelIndexes()[random_y_pred]
+        else: 
+            # y_pred is not None... 
+            if self.schema.getLabelIndexes() is not None:
+                # Find the index of the corresponding value (it y_pred is a value instead of an index). 
+                if y_pred in self.schema.getLabelValues():
+                    y_pred = self.schema.getLabelValues().index(y_pred)
+        
+        prediction_array[y_pred] += 1        
+        self.moa_basic_evaluator.addResult(example, prediction_array)
+
+        self.instances_seen += 1
+        
+        if self.window_size is not None and self.instances_seen % self.window_size == 0:
+            performance_values = [measurement.getValue() for measurement in self.moa_basic_evaluator.getPerformanceMeasurements()]
+            self.result_windows.append(performance_values)
+
+    def measurements(self):
+        performance_measurements = self.moa_basic_evaluator.getPerformanceMeasurements()
+        performance_names = ["".join(measurement.getName()) for measurement in performance_measurements]
+        return performance_names
+
+    def results(self):
+        return [measurement.getValue() for measurement in self.moa_basic_evaluator.getPerformanceMeasurements()]
+
+    def windowed_results(self):
+        return pd.DataFrame(self.result_windows, columns=self.measurements())
+    
+    def accuracy(self):
+        index = self.measurements().index('classifications correct (percent)')
+        return self.results()[index]
+
+    def kappa(self):
+        index = self.measurements().index('Kappa Statistic (percent)')
+        return self.results()[index]
+
+    def kappaT(self):
+        index = self.measurements().index('Kappa Temporal Statistic (percent)')
+        return self.results()[index]
+
+    def kappaM(self):
+        index = self.measurements().index('Kappa M Statistic (percent)')
+        return self.results()[index]
+
+
+class ClassificationWindowedEvaluator(ClassificationEvaluator):
+    def __init__(self, schema=None, window_size=1000, recall_per_class=False, precision_per_class=False, 
+                 f1_precision_recall=False, f1_per_class=False):
+        self.moa_evaluator = WindowClassificationPerformanceEvaluator()
+        self.moa_evaluator.widthOption.setValue(window_size)
+        
+        super().__init__(schema=schema, window_size=window_size, recall_per_class=recall_per_class, 
+                         precision_per_class=precision_per_class, f1_precision_recall=f1_precision_recall, 
+                         f1_per_class=f1_per_class, moa_evaluator=self.moa_evaluator)
+
+
 ## Function to abstract the test and train loop using MOA
-def test_train_loop_MOA(stream, learner, maxInstances=1000, sampleFrequency=100, evaluator=BasicClassificationPerformanceEvaluator()):
+def test_train_loop_MOA(stream, learner, maxInstances=1000, sampleFrequency=100, evaluator=None):
     # Start measuring time
     start_wallclock_time, start_cpu_time = start_time_measuring()
     
@@ -44,7 +208,9 @@ def test_train_loop_MOA(stream, learner, maxInstances=1000, sampleFrequency=100,
     learner.setModelContext(stream.getHeader())
 
     # evaluator.recallPerClassOption.set()
-    evaluator.prepareForUse()
+    if evaluator is None:
+        evaluator = BasicClassificationPerformanceEvaluator()
+        evaluator.prepareForUse()
     
     data = []
     performance_names = []
@@ -79,7 +245,7 @@ def test_train_loop_MOA(stream, learner, maxInstances=1000, sampleFrequency=100,
 
 
 ## Function to abstract the test and train loop using RIVER
-def test_train_loop_RIVER(model, dataset, maxInstances=1000, sampleFrequency=100):
+def test_train_loop_RIVER(dataset, model, maxInstances=1000, sampleFrequency=100):
     # Start measuring time
     start_wallclock_time, start_cpu_time = start_time_measuring()
 
