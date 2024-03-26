f33d9a1a7f7264488871e8b415cb6a8be383f5d1
Marco Heyden <heyden@i40pool.ipd.kit.edu>
2024-03-26 22:58:47 +0100

    Setup tests for HT and EFDT

--- tests/test_EFDT.py
+++ tests/test_EFDT.py
@@ -7,32 +7,14 @@ import pytest
 @pytest.mark.parametrize(
     "stream, expectation",
     [
-        (ElectricityTiny(), 46.0),
-        (CovtypeTiny(), 46.0),
+        (ElectricityTiny(), 47.5),
+        (CovtypeTiny(), 53.2),
     ],
     ids=["ElectricityTiny", "CovtypeTiny"]
 )
 def test_EFDT(stream, expectation):
     # The optimizer steps are set to 10 to speed up the test
-    learner = EFDT(
-        schema=stream.schema,
-        grace_period=201,
-        min_samples_reevaluate=21,
-        # split_criterion="gini",
-        confidence=1e-3,
-        tie_threshold=0.055,
-        # leaf_prediction="mc",
-        nb_threshold=1,
-        # numeric_attribute_observer="FIMTDDNumericAttributeClassObserver",
-        binary_split=True,
-        min_branch_fraction=0.02,
-        max_share_to_split=0.98,
-        max_byte_size=33554434,
-        memory_estimate_period=1000001,
-        stop_mem_management=True,
-        remove_poor_attrs=True,
-        disable_prepruning=False,
-    )
+    learner = EFDT(schema=stream.schema)
     assert_ssl_evaluation(
         learner,
         stream,
