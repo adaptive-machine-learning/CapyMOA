a4c206af1f8f397fbb6d9fb84aaa48407ce6674e
Anton Lee <anton.j.15.le@gmail.com>
2024-04-11 16:49:46 +1200

    Add `PassiveAggressiveClassifier`

--- src/capymoa/learner/classifier/__init__.py
+++ src/capymoa/learner/classifier/__init__.py
@@ -1,5 +1,13 @@
 from .classifiers import AdaptiveRandomForest, OnlineBagging, AdaptiveRandomForest
 from .efdt import EFDT
+from .sklearn import PassiveAggressiveClassifier
 from .hoeffding_tree import HoeffdingTree
 
-__all__ = ["AdaptiveRandomForest", "OnlineBagging", "AdaptiveRandomForest", "EFDT", "HoeffdingTree"]
+__all__ = [
+    "AdaptiveRandomForest",
+    "OnlineBagging",
+    "AdaptiveRandomForest",
+    "EFDT",
+    "HoeffdingTree",
+    "PassiveAggressiveClassifier",
+]
