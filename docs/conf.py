2be493bd62671db9ba8aff8b508557731fb4bd1b
Anton Lee <anton.j.15.le@gmail.com>
2024-04-17 11:46:57 +1200

    Fix documentation build

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ extlinks = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_book_theme"
+html_theme = "pydata_sphinx_theme"
 html_static_path = ['_static']
 
 # Setup symbolic links for notebooks
---
+++ docs/contributing/index.rst
@@ -0,0 +1,10 @@
+Contributing
+============
+This part of the documentation is for developers and contributors.
+
+.. toctree::
+   :maxdepth: 2
+
+   learners
+   tests
+   docs
