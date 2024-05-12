1bb4a9db36b01b9ce86e34df1990094615ce2af0
Justin Liu <justin.l@waikato.ac.nz>
2024-05-12 19:29:17 +1200

    Add docs.

--- src/capymoa/anomaly/half_space_trees.py
+++ src/capymoa/anomaly/half_space_trees.py
@@ -1,12 +1,27 @@
 from capymoa.base import (
     MOAClassifier,
-    _extract_moa_learner_CLI,
 )
 
 from moa.classifiers.oneclass import HSTrees as _MOA_HSTrees
 
 
 class HalfSpaceTrees(MOAClassifier):
+    """ Half-Space Trees
+
+    This class implements the Half-Space Trees (HS-Trees) algorithm, which is
+    an ensemble anomaly detector capable of adapting to concept drift.
+
+    HS-Trees is implemented in MOA (Massive Online Analysis) and provides several
+    parameters for customization.
+
+    Reference:
+    Tan, S. C., Ting, K. M., & Liu, T. F. (2011, June). Fast anomaly detection for streaming data.
+    In Twenty-second international joint conference on artificial intelligence.
+    <https://citeseerx.ist.psu.edu/document?repid=rep1&type=pdf&doi=73b6b7d9e7e225719ad86234927a3b60a4a873c0>
+
+    Example usage:
+
+    """
     def __init__(
         self, schema=None, CLI=None, random_seed=1, window_size=100, number_of_trees=25, max_depth=15
     ):
