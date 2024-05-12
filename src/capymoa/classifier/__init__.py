4f2bfe14d3420718233fa72a0fca95f7c5fbefe9
Spencer Sun <spencer@Spencers-MacBook-Pro.local>
2024-05-12 16:34:59 +1200

    feat: add samknn, hat, dwm wrappers

--- src/capymoa/classifier/__init__.py
+++ src/capymoa/classifier/__init__.py
@@ -12,6 +12,9 @@ from ._majority_class import MajorityClass
 from ._no_change import NoChange
 from ._online_smooth_boost import OnlineSmoothBoost
 from ._srp import StreamingRandomPatches
+from ._samknn import SAMkNN
+from ._dynamic_weighted_majority import DynamicWeightedMajority
+from ._hoeffding_adaptive_tree import HoeffdingAdaptiveTree
 
 __all__ = [
     "AdaptiveRandomForestClassifier",
@@ -27,5 +30,8 @@ __all__ = [
     "MajorityClass",
     "NoChange",
     "OnlineSmoothBoost",
-    "StreamingRandomPatches"
+    "StreamingRandomPatches",
+    "SAMkNN",
+    "DynamicWeightedMajority",
+    "HoeffdingAdaptiveTree",
 ]
