bf3f7cef441f54d1c4d3e72e9c015989cf8c969a
Spencer Sun <spencer@Spencers-MacBook-Pro.local>
2024-05-14 14:34:37 +1200

    feat: update plotting regression/prediction interval

--- src/capymoa/base.py
+++ src/capymoa/base.py
@@ -325,6 +325,7 @@ class MOARegressor(Regressor):
 
         self.moa_learner.prepareForUse()
         self.moa_learner.resetLearning()
+        self.moa_learner.setModelContext(self.schema.get_moa_header())
 
     def __str__(self):
         full_name = str(self.moa_learner.getClass().getCanonicalName())
