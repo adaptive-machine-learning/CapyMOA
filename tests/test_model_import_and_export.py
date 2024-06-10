a21d7fb6b0a394d562238f0c586f9941135366e0
Justin Liu <justinuliu@gmail.com>
2024-06-10 17:16:15 +1200

    test: rename name of unit test

--- tests/test_model_import_and_export.py
+++ tests/test_model_import_and_export.py
@@ -6,30 +6,26 @@ from capymoa.evaluation import prequential_evaluation
 import os
 
 
-def test_save_and_load_model_with_valid_model():
-    # Arrange
+def test_save_and_load_model():
+   
     stream = ElectricityTiny()
     schema = stream.get_schema()
     learner = AdaptiveRandomForestClassifier(schema)
     prequential_evaluation(stream, learner, max_instances=1000)
     filename = 'test_model.pkl'
 
-    # Act
     save_model(learner, filename)
     loaded_model = load_model(filename)
 
-    # Assert
     assert isinstance(loaded_model, AdaptiveRandomForestClassifier)
     assert loaded_model != learner
     assert loaded_model.base_learner == learner.base_learner
     os.remove(filename)
 
 
-def test_load_model_with_non_existent_file():
-    # Arrange
+def test_load_model_missing_file():
     filename = 'non_existent_file.pkl'
 
-    # Act & Assert
     with pytest.raises(FileNotFoundError):
         load_model(filename)
 
