83ce1685c3337480001fa9294e4c00e8edff2b4d
Filippo Leveni <filippo.leveni@gmail.com>
2024-07-11 11:50:22 +0200

    refactor(test_anomaly_detectors): change 'window_size' parameter value of OnlineIsolationForest to 100

--- tests/test_anomaly_detectors.py
+++ tests/test_anomaly_detectors.py
@@ -18,7 +18,7 @@ from capymoa.stream._stream import Schema
     "learner_constructor,auc,cli_string",
     [
         (partial(HalfSpaceTrees, window_size=100, number_of_trees=25, max_depth=15), 0.54, None),
-        (partial(OnlineIsolationForest, num_trees=32, max_leaf_samples=32, window_size=2048), 0.52, None),
+        (partial(OnlineIsolationForest, window_size=100, num_trees=32, max_leaf_samples=32), 0.49, None),
     ],
     ids=[
         "HalfSpaceTrees",
