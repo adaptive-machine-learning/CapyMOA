44340149b5ad335580eeb2498c6d9808d59de7c2
Anton Lee <anton.j.15.le@gmail.com>
2024-07-25 13:12:30 +1200

    docs(drift): update drift detector docs

--- src/capymoa/drift/base_detector.py
+++ src/capymoa/drift/base_detector.py
@@ -1,12 +1,12 @@
 from abc import ABC, abstractmethod
+from typing import Any, Dict
+from typing_extensions import override
 
 from jpype import _jpype
 
 
 class BaseDriftDetector(ABC):
-    """ Drift Detector
-
-    """
+    """Drift Detector"""
 
     def __init__(self):
         super().__init__()
@@ -18,27 +18,14 @@ class BaseDriftDetector(ABC):
         self.data = []
         self.idx = 0
 
-        self.params = {}
+    @abstractmethod
+    def get_params(self) -> Dict[str, Any]:
+        """Get the hyper-parameters of the drift detector."""
 
-    def get_params(self):
-        pass
+    def reset(self, clean_history: bool = False) -> None:
+        """Reset the drift detector.
 
-    def reset(self, clean_history: bool = False):
-        """ reset
-
-        Resets the drift detector
-
-        Parameters
-        ----------
-        clean_history: bool
-            Whether to reset detection history
-
-        Returns
-        -------
-        BaseDriftDetector
-            self, optional
-
-
+        :param clean_history: Whether to reset detection history, defaults to False
         """
         self.in_concept_change = False
         self.in_warning_zone = False
@@ -50,62 +37,33 @@ class BaseDriftDetector(ABC):
             self.idx = 0
 
     @abstractmethod
-    def add_element(self, input_value):
-        """ add_element
+    def add_element(self, element: float) -> None:
+        """Update the drift detector with a new input value.
 
-        Adds the relevant data from a sample into the change detector.
-
-        Parameters
-        ----------
-        input_value: Not defined
-            Whatever input value the change detector takes.
-
-        Returns
-        -------
-        BaseDriftDetector
-            self, optional
-
+        :param element: A value to update the drift detector with. Usually,
+            this is the prediction error of a model.
         """
         raise NotImplementedError
 
-    def detected_change(self):
-        """ detected_change
-
-        This function returns whether concept drift was detected or not.
-
-        Returns
-        -------
-        bool
-            Whether concept drift was detected or not.
-
-        """
+    def detected_change(self) -> bool:
+        """Is the detector currently detecting a concept drift?"""
         return self.in_concept_change
 
-    def detected_warning(self):
-        """ detected_warning_zone
-
-        If the change detector supports the warning zone, this function will return
-        whether it's inside the warning zone or not.
-
-        Returns
-        -------
-        bool
-            Whether the change detector is in the warning zone or not.
-
-        """
+    def detected_warning(self) -> bool:
+        """Is the detector currently warning of an upcoming concept drift?"""
         return self.in_warning_zone
 
 
 class MOADriftDetector(BaseDriftDetector):
     """
     A wrapper class for using MOA (Massive Online Analysis) drift detectors in CapyMOA.
-
-    Attributes:
-    - moa_detector: The MOA detector object or class identifier.
-    - CLI: The command-line interface (CLI) configuration for the MOA drift detector.
     """
 
     def __init__(self, moa_detector, CLI=None):
+        """
+        :param moa_detector: The MOA detector object or class identifier.
+        :param CLI: The command-line interface (CLI) configuration for the MOA drift detector, defaults to None
+        """        
         super().__init__()
 
         self.CLI = CLI
@@ -133,7 +91,8 @@ class MOADriftDetector(BaseDriftDetector):
     def CLI_help(self):
         return str(self.moa_detector.getOptions().getHelpString())
 
-    def add_element(self, element):
+    @override
+    def add_element(self, element: float) -> None:
         self.moa_detector.input(element)
         self.data.append(element)
         self.idx += 1
@@ -147,24 +106,7 @@ class MOADriftDetector(BaseDriftDetector):
         if self.in_concept_change:
             self.detection_index.append(self.idx)
 
-    def detected_change(self):
-        return self.in_concept_change
-
-    def detected_warning(self):
-        return self.in_warning_zone
-
-    def get_params(self):
+    @override
+    def get_params(self) -> Dict[str, Any]:
         options = list(self.moa_detector.getOptions().getOptionArray())
-        self.params = {opt.getName(): opt.getValueAsCLIString() for opt in options}
-        return self.params
-
-    def reset(self, clean_history: bool = False):
-        self.in_concept_change = False
-        self.in_warning_zone = False
-        self.moa_detector.resetLearning()
-
-        if clean_history:
-            self.detection_index = []
-            self.warning_index = []
-            self.data = []
-            self.idx = 0
+        return {opt.getName(): opt.getValueAsCLIString() for opt in options}
