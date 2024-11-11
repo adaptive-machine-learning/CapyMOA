09a80ebc5d8681a42fca9d1d97365d68f97ab554
heymarco <heydenmarco48@gmail.com>
2024-11-11 04:17:57 +0100

    Pipelines V2 (#200)
    Co-authored-by: Heitor Murilo Gomes <heitor_murilo_gomes@yahoo.com.br>

--- src/capymoa/stream/preprocessing/__init__.py
+++ src/capymoa/stream/preprocessing/__init__.py
@@ -1,14 +1,19 @@
 from .pipeline import (
-    BasePipeline, ClassifierPipeline, RegressorPipeline
+    BasePipeline, ClassifierPipeline, RegressorPipeline, ClassifierPipelineElement,
+    TransformerPipelineElement, RegressorPipelineElement, DriftDetectorPipelineElement, PipelineElement,
+    RandomSearchClassifierPE
 )
 from .transformer import (
     Transformer, MOATransformer
 )
 
 __all__ = [
-    "BasePipeline",
-    "ClassifierPipeline",
-    "RegressorPipeline",
     "Transformer",
-    "MOATransformer"
+    "MOATransformer",
+    "PipelineElement",
+    "ClassifierPipelineElement",
+    "TransformerPipelineElement",
+    "RegressorPipelineElement",
+    "DriftDetectorPipelineElement",
+    "RandomSearchClassifierPE"
 ]
