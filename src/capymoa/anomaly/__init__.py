a91f372f6b9547dbb7b6c53d8990471585484096
Justin Liu <justinuliu@gmail.com>
2025-04-20 20:34:22 +1200

    feat(anomaly): streaming isolation forest

--- src/capymoa/anomaly/__init__.py
+++ src/capymoa/anomaly/__init__.py
@@ -2,10 +2,12 @@ from ._half_space_trees import HalfSpaceTrees
 from ._online_isolation_forest import OnlineIsolationForest
 from ._autoencoder import Autoencoder
 from ._stream_rhf import StreamRHF
+from ._streaming_isolation_forest import StreamingIsolationForest
 
 __all__ = [
     "HalfSpaceTrees",
     "OnlineIsolationForest",
     "Autoencoder",
     "StreamRHF",
+    "StreamingIsolationForest",
 ]
