c63d85993eea17ffe50027fafb231f7d075ee843
Heitor Murilo Gomes <heitor_murilo_gomes@yahoo.com.br>
2025-09-12 08:50:28 +1200

    feat: update moa.jar

--- tests/test_moajar.py
+++ tests/test_moajar.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from hashlib import sha256
 import capymoa
 
-_MOA_JAR_HASH = "b14be3c1df87aa5bf37f24c9a35258ab1f9a941897e61294701c43c0141dc2b7"
+_MOA_JAR_HASH = "66e9ad8a7b7607d68b1477e7ccd675570b940e0f09486b080c07bf2fcecf26c3"
 
 
 def test_imports() -> None:
