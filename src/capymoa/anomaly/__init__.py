2153de78a8a0a73f090f5535fb7547a09c3b9811
Nuwan Gunasekara <ng198002@gmail.com>
2025-09-12 07:56:12 +0200

    Update SGBR branch (#291)
    Co-authored-by: Justin Liu <justinuliu@gmail.com>
    Co-authored-by: Anton Lee <anton.j.15.le@gmail.com>
    Co-authored-by: Tommaso Crippa <crippa.tommaso@gmail.com>
    Co-authored-by: Heitor Murilo Gomes <heitor_murilo_gomes@yahoo.com.br>

--- src/capymoa/anomaly/__init__.py
+++ src/capymoa/anomaly/__init__.py
@@ -3,6 +3,7 @@ from ._online_isolation_forest import OnlineIsolationForest
 from ._autoencoder import Autoencoder
 from ._stream_rhf import StreamRHF
 from ._streaming_isolation_forest import StreamingIsolationForest
+from ._robust_random_cut_forest import RobustRandomCutForest
 
 __all__ = [
     "HalfSpaceTrees",
@@ -10,4 +11,5 @@ __all__ = [
     "Autoencoder",
     "StreamRHF",
     "StreamingIsolationForest",
+    "RobustRandomCutForest",
 ]
