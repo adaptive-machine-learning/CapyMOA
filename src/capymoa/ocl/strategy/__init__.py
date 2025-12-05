b44b375219e18042f028c8bd0384223d355bd376
Anton Lee <anton.lee@vuw.ac.nz>
2025-12-05 15:46:43 +1300

    feat(ocl): add Repeated Augmented Rehearsal (RAR)

--- src/capymoa/ocl/strategy/__init__.py
+++ src/capymoa/ocl/strategy/__init__.py
@@ -2,5 +2,6 @@ from ._experience_replay import ExperienceReplay
 from ._slda import SLDA
 from ._ncm import NCM
 from ._gdumb import GDumb
+from ._rar import RAR
 
-__all__ = ["ExperienceReplay", "SLDA", "NCM", "GDumb"]
+__all__ = ["ExperienceReplay", "SLDA", "NCM", "GDumb", "RAR"]
